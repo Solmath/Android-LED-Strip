453f9ce5ae08abf02a49033c49449906e9beb0e7
Solmath <thommy86@gmail.com>
2019-03-28 01:09:02 +0100

    Start implementation of device list

---
+++ app/src/main/java/com/thomaspfeiffer/ledstrip/Device.java
@@ -0,0 +1,49 @@
+package com.thomaspfeiffer.ledstrip;
+
+import java.lang.reflect.Array;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+public class Device {
+    private InetAddress mIP;
+    private String mName;
+    private ledType mType;
+
+    public Device(InetAddress IP, String Name){
+        mIP = IP;
+        mName = Name;
+        mType =ledType.RGB;
+    }
+
+    public InetAddress getIP(){
+        return mIP;
+    }
+
+    public String getName(){
+        return mName;
+    }
+
+    private static int lastDeviceId = 0;
+
+    // Dummy method to create a data source
+    // Tutorial: https://guides.codepath.com/android/using-the-recyclerview
+    public static ArrayList<Device> createDeviceList(int numDevices) throws UnknownHostException {
+        ArrayList<Device> devices = new ArrayList<Device>();
+
+        for (int i = 1; i <= numDevices; i++){
+            byte[] ipAddr = new byte[]{127, 0, 0, 1};
+            devices.add(new Device(InetAddress.getByAddress(ipAddr), "Gerät " + ++lastDeviceId));
+        }
+
+        return devices;
+    }
+}
+
+enum ledType
+{
+    RGB,
+    RGBW,
+    CCT;
+}
