9f845298f7ca18a356f862eeee167781554b39cf
Solmath <thommy86@gmail.com>
2019-05-05 21:53:46 +0200

    Remove precompiled headers from repository

--- nodeMCU/LED_Strip_pIO/src/main.cpp
+++ nodeMCU/LED_Strip_pIO/src/main.cpp
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library
 #include <ESP8266SSDP.h>
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
-#include <ESP8266mDNS.h>;
+#include <ESP8266mDNS.h>
 
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
